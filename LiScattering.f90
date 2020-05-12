44d07024352d4a1e96d940363f92f81d88638194
quantumwave <nmehta@trinity.edu>
2020-05-12 11:07:47 -0500

    initial commit

---
+++ LiScattering.f90
@@ -0,0 +1,39 @@
+program Li2
+  use units
+  implicit none
+  integer ISTATE, IMN1, IMN2, NPP, i
+  double precision, allocatable :: XO(:), VSinglet(:), VTriplet(:), RM2(:)
+  double precision VLIM,xmin,xmax,dx
+  
+  NPP = 100
+  VLIM = 0d0
+  
+  
+  allocate(XO(NPP),VSinglet(NPP),VTriplet(NPP),RM2(NPP))
+  IMN1 = 6
+  IMN2 = 6
+  xmin = 1.0d0
+  xmax = 15.0d0
+  dx = (xmax-xmin)/(dble(NPP-1))
+  do i=1, NPP
+     XO(i) = xmin + (i-1)*dx
+  enddo
+  ISTATE = 1                ! Find the "singlet" X(^1\Sigma_g^+) curve
+  ! Call the Le Roy routine to generate the MLR potential
+  call POTGENLI2(ISTATE,IMN1,IMN2,NPP,VLIM,XO,RM2,VSinglet)
+
+  ! print singlet potential to fort.10
+  do i=1, NPP
+     write(10,*) XO(i)*AngstrominBohr, VSinglet(i)*Hartreeininvcm
+  enddo
+  
+  ISTATE = 2                !Find the triplet potential
+  ! Call the Le Roy routine to generate the MLR potential
+  call POTGENLI2(ISTATE,IMN1,IMN2,NPP,VLIM,XO,RM2,VTriplet)
+  do i=1, NPP
+     write(30,*) XO(i)*AngstrominBohr, VTriplet(i)*Hartreeininvcm
+  enddo
+  
+  
+end program Li2
+
---
+++ units.f90
@@ -0,0 +1,24 @@
+
+module units
+  
+  real*8, parameter :: Kininvcm = 1.4387862961655296d0
+  real*8, parameter :: mKininvcm = 1.4387862961655296d3
+  real*8, parameter :: Hartreeininvcm = 0.0000045563352812122295d0
+  real*8, parameter :: eVininvcm = 0.00012398425731484318d0
+  real*8, parameter :: BohrinAngstrom = 0.529177210903d0
+  real*8, parameter :: AngstrominBohr = 1d0/BohrinAngstrom
+
+!!$  real*8, parameter ::  AnstperBohr,HartreeperJoule,JouleperHartree,hSI,hHartreesec,hbarcMeVfm,hbarceVnm
+!!$  
+!!$  Kperinvcm = 
+!!$  mKperinvcm = Kperinvcm*1d3
+!!$  Hartreeperinvcm = 0.0000045563352812122295d0
+!!$  eVperinvcm = 0.00012398425731484318d0
+!!$  BohrperAngstrom = 0.529177210903d0
+!!$  Angstromperbohr = 1d0/bohrperAngstrom
+!!$  HartreeperJoule = 2.2937122783963d17
+!!$  JouleperHartree = 1d0/HartreeperJoule
+!!$  hSI = 6.62607015d-34
+!!$  hHartreeSec = hSI*JouleperHartree
+
+end module units
